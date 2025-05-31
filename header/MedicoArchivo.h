9ec239e68dae2b7e1886abb2c0b55ebdd41fdf14
svanderstichel <vanderstichel@gmail.com>
2025-05-31 13:32:07 -0300

    Se anadio la logica de archivo al medico.

--- header/MedicoArchivo.h
+++ header/MedicoArchivo.h
@@ -3,12 +3,18 @@
 
 class MedicoArchivo{
     public:
+        ///constructor
         MedicoArchivo();
+        ///get
         int getPosicion(int dni);
         int getCantidadRegistros();
+        ///leer un registro
         Medico leer(int pos);
+        ///leer todos los registros en un vector
         void leer(int cantidadRegistros,Medico *vec);
+        ///escribir un registro
         bool escribir(Medico reg);
+        ///modificar un registro
         bool escribir(int pos,Medico reg);
     private:
         char _nombreArchivo[50];
