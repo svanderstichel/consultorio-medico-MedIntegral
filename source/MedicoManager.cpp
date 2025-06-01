f6e23958603e9cccf4bc1a0292d435c3faac468b
svanderstichel <vanderstichel@gmail.com>
2025-06-01 13:44:18 -0300

    Se genero la logica ABML Medico.

--- source/MedicoManager.cpp
+++ source/MedicoManager.cpp
@@ -73,6 +73,7 @@ void MedicoManager::listar(){
             << vec[i].getFechaNacimiento().getAnio() << endl;
         }
     }
+    delete[] vec;
 }
 void MedicoManager::baja(){
     Medico reg;
@@ -95,4 +96,134 @@ void MedicoManager::baja(){
     }
 
 }
-void MedicoManager::modificar(){return;}
+void MedicoManager::modificar(){
+    Medico reg;
+    int dni;
+    int pos;
+    int opc;
+
+    cout << "\nIngrese el DNI del medico: ";
+    cin >> dni;
+    cin.ignore();
+
+    pos = archivo.getPosicion(dni);
+    if(pos==-1){cout << "\nEl registro no existe en el disco.\n";}
+
+    if(pos!=-1){
+    reg = archivo.leer(pos);
+    cout << reg.getApellido() << "\t"
+        << reg.getNombre() << "\t"
+        << reg.getEmail() << "\t"
+        << reg.getTelefono() << "\t"
+        << reg.getCodigoEspecialidad() << "\t"
+        << reg.getFechaNacimiento().getDia() << "/"
+        << reg.getFechaNacimiento().getMes() << "/"
+        << reg.getFechaNacimiento().getAnio() << endl;
+
+    cout << "\n1 - Apellido";
+    cout << "\n2 - Nombre";
+    cout << "\n3 - Email";
+    cout << "\n4 - Telefono";
+    cout << "\n5 - Codigo de especialidad";
+    cout << "\n6 - Fecha de nacimiento";
+    cout << "\nIndique el atributo a modificar: ";
+    cin >> opc;
+    cin.ignore();
+
+    switch(opc){
+    case 1:{
+        {string apellido = reg.getApellido();
+
+        do{
+        cout << "Ingrese nuevo apellido: ";
+        getline(cin,apellido);
+        reg.setApellido(apellido);}
+        while(apellido!=reg.getApellido());
+
+        if(archivo.escribir(pos,reg)){
+            cout << "\nRegistro modificado correctamente.\n";
+        } else{
+            cout << "\nSe produjo un error de escritura en disco.\n";}
+
+        break;}
+    case 2:
+         {string nombre = reg.getNombre();
+
+        do{
+        cout << "Ingrese nuevo nombre: ";
+        getline(cin,nombre);
+        reg.setNombre(nombre);}
+        while(nombre!=reg.getNombre());
+
+        if(archivo.escribir(pos,reg)){
+            cout << "\nRegistro modificado correctamente.\n";
+        } else{
+            cout << "\nSe produjo un error de escritura en disco.\n";}
+
+        break;}
+    case 3:
+        {string email = reg.getEmail();
+
+        do{
+        cout << "Ingrese nuevo email: ";
+        getline(cin,email);
+        reg.setEmail(email);}
+        while(email!=reg.getEmail());
+
+        if(archivo.escribir(pos,reg)){
+            cout << "\nRegistro modificado correctamente.\n";
+        } else{
+            cout << "\nSe produjo un error de escritura en disco.\n";}
+        break;}
+    case 4:
+        {string telefono = reg.getTelefono();
+
+        do{
+        cout << "Ingrese nuevo telefono: ";
+        getline(cin,telefono);
+        reg.setTelefono(telefono);}
+        while(telefono!=reg.getTelefono());
+
+        if(archivo.escribir(pos,reg)){
+            cout << "\nRegistro modificado correctamente.\n";
+        } else{
+            cout << "\nSe produjo un error de escritura en disco.\n";}
+
+        break;}
+    case 5:
+        {int codigoEspecialidad = reg.getCodigoEspecialidad();
+
+        do{
+        cout << "Ingrese nuevo codigo de especialidad: ";
+        cin >> codigoEspecialidad;
+        cin.ignore();
+        reg.setCodigoEspecialidad(codigoEspecialidad);}
+        while(codigoEspecialidad!=reg.getCodigoEspecialidad());
+
+        if(archivo.escribir(pos,reg)){
+            cout << "\nRegistro modificado correctamente.\n";
+        } else{
+            cout << "\nSe produjo un error de escritura en disco.\n";}
+
+        break;}
+    case 6:
+        {FechaHora fechaNacimiento;
+
+        cout << "\nIngrese nueva fecha de nacimiento\n";
+        fechaNacimiento.cargarFecha();
+        reg.setFechaNacimiento(fechaNacimiento);}
+
+        if(archivo.escribir(pos,reg)){
+            cout << "\nRegistro modificado correctamente.\n";
+        } else{
+            cout << "\nSe produjo un error de escritura en disco.\n";}
+
+        break;}
+    default:
+        cout << "\nOpcion invalida.\n";
+        break;
+            }
+        }
+    }
+
+
