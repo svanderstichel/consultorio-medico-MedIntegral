0549a08fcfde0352e37ebc9190506e88e9e1902b
svanderstichel <vanderstichel@gmail.com>
2025-06-16 18:12:28 -0300

    cambio menor.

--- header/TurnoArchivo.h
+++ header/TurnoArchivo.h
@@ -11,7 +11,6 @@ class TurnoArchivo{
         int getCantidadRegistros();
         ///leer un registro
         Turno leer(int pos);
-        Turno Leer(int posicion);
         ///leer todos los registros en un vector
         void leer(int cantidadRegistros,Turno *vec);
         ///escribir un registro
